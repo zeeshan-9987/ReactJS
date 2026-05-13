29253943a9b9aa28c6af75d9117c722ef03bae53
zeeshan-9987 <zeeshanansari78794@gmail.com>
2026-05-13 18:52:18 +0530

    Made My Portfolio Website in React.js

--- Portfolio/src/components/Navbar.jsx
+++ Portfolio/src/components/Navbar.jsx
@@ -40,15 +40,35 @@ export default function Navbar() {
 
       <ul className={open ? "nav-links active" : "nav-links"}>
 
-        <li><a href="#home">Home</a></li>
+        <li>
+  <a href="#home" onClick={() => setOpen(false)}>
+    Home
+  </a>
+</li>
 
-        <li><a href="#about">About</a></li>
+<li>
+  <a href="#about" onClick={() => setOpen(false)}>
+    About
+  </a>
+</li>
 
-        <li><a href="#skills">Skills</a></li>
+<li>
+  <a href="#skills" onClick={() => setOpen(false)}>
+    Skills
+  </a>
+</li>
 
-        <li><a href="#projects">Projects</a></li>
+<li>
+  <a href="#projects" onClick={() => setOpen(false)}>
+    Projects
+  </a>
+</li>
 
-        <li><a href="#contact">Contact</a></li>
+<li>
+  <a href="#contact" onClick={() => setOpen(false)}>
+    Contact
+  </a>
+</li>
 
       </ul>
 
